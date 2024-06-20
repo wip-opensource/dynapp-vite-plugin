07713ac480c33a12e24231f5155541ffa2c13531
Jonatan Robertsson <jonatanr@wip.se>
2024-06-20 07:42:36 +0200

    testing avoiding node_modules

--- update-package-json.mjs
+++ update-package-json.mjs
@@ -7,6 +7,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 function findPackageJson(currentDir) {
+  if (currentDir.includes('node_modules')) {
+    return null;
+  }
+
   const packageJsonPath = path.join(currentDir, 'package.json');
   console.log(`Looking for package.json at ${packageJsonPath}`);
 
