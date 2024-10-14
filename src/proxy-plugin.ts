d6fdcae1c78fc99d57efbc266cbbae26c5c9a3e6
Daniel Mattsson <danielm@wip.se>
2024-10-14 10:22:39 +0200

    Change in whitespace

--- src/proxy-plugin.ts
+++ src/proxy-plugin.ts
@@ -13,11 +13,11 @@ function getProxyTarget(): string {
   const runapp = config.runapp;
 
   // Construct the target URL based on the JSON values
-  
-  const targetUrl = rungroup && runapp 
-  ? `/dynapp-server/public/${rungroup}/${runapp}` 
+
+  const targetUrl = rungroup && runapp
+  ? `/dynapp-server/public/${rungroup}/${runapp}`
   : `/dynapp-server/public/${group}/${app}`;
-  
+
   console.log('Proxy Target URL:', targetUrl);
 
   return targetUrl;
