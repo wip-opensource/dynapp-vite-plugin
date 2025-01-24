3966a797057e3538485270c2d970f0241e692c71
Daniel Mattsson <danielm@wip.se>
2025-01-24 15:37:26 +0100

    Removed unused variables

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,8 @@ import path from 'path';
 import fs from 'fs-extra';
 import ora from 'ora';
 import * as readline from 'readline';
-import { fileURLToPath } from 'url';
 import { DynappConfig } from './interface.js';
 import { getDynappConfig } from './util.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const getDistFiles = (folder: string) => {
   const result: string[] = [];
