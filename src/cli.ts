06648d96937fc144b946bd22858ea3343bc37688
Daniel Mattsson <danielm@wip.se>
2025-01-24 15:36:03 +0100

    Removed ts-node from cli as it doesn't seem needed

--- src/cli.ts
+++ src/cli.ts
@@ -1,15 +1,10 @@
 #!/usr/bin/env node
 
-import { register } from 'ts-node';
 import { fileURLToPath, pathToFileURL } from 'url';
 import { dirname, resolve } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-register({
-  project: resolve(__dirname, '../tsconfig.json')
-});
 
 const mainPath = resolve(__dirname, './main.js');
 import(pathToFileURL(mainPath).toString()).catch(console.error);
